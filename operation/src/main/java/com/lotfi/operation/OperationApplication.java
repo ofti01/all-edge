af2336033f821cfff62ee138bfa9a42191a4a4d5
ofti01 <oftizeidi@gmail.com>
2024-02-25 15:02:47 +0100

    create docker compose file

--- operation/src/main/java/com/lotfi/operation/OperationApplication.java
+++ operation/src/main/java/com/lotfi/operation/OperationApplication.java
@@ -1,7 +1,6 @@
 package com.lotfi.operation;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 
 @SpringBootApplication(
